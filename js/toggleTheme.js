19bac0927eefc344314309afddb91256b3a0153c
PavelBeloded <beloded_2007@bk.ru>
2025-04-25 17:27:22 +0300

    theme saving

--- js/toggleTheme.js
+++ js/toggleTheme.js
@@ -2,32 +2,46 @@ const footer = document.querySelector(".footer");
 const headerEl = document.querySelector(".header");
 const sections = document.querySelectorAll("section");
 
+function isDark() {
+  return !document.body.classList.contains("light");
+}
+
+const theme = localStorage.getItem("theme");
+if (!theme) {
+  localStorage.setItem("theme", "dark");
+} else if (isDark() && theme != "dark") {
+  toggleTheme();
+} else if (!isDark() && theme != "light") {
+  toggleTheme();
+}
+
 function toggleTheme() {
-    const isDark = !document.body.classList.contains('light');
+  document.querySelectorAll("#iconWrapper").forEach((wrapper) => {
+    const sun = wrapper.querySelector("#sunIcon");
+    const moon = wrapper.querySelector("#moonIcon");
 
-    document.querySelectorAll("#iconWrapper").forEach((wrapper)=>{
-        const sun = wrapper.querySelector('#sunIcon');
-        const moon = wrapper.querySelector('#moonIcon');
-    
-        if (isDark) {
-          sun.classList.remove('show');
-          sun.classList.add('hide');
-          moon.classList.remove('hide');
-          moon.classList.add('show');
-        } else {
-          moon.classList.remove('show');
-          moon.classList.add('hide');
-          sun.classList.remove('hide');
-          sun.classList.add('show');
-        }
-    })
-    
+    if (isDark()) {
+      sun.classList.remove("show");
+      sun.classList.add("hide");
+      moon.classList.remove("hide");
+      moon.classList.add("show");
+    } else {
+      moon.classList.remove("show");
+      moon.classList.add("hide");
+      sun.classList.remove("hide");
+      sun.classList.add("show");
+    }
+  });
 
-    document.body.classList.toggle("light");
-    headerEl.classList.toggle("light");
-    footer.classList.toggle("light");
-    document.querySelector(".modal").classList.toggle("light");
-    sections.forEach((element)=>{
-        element.classList.toggle("light");
-    })
+  document.body.classList.toggle("light");
+  headerEl.classList.toggle("light");
+  footer.classList.toggle("light");
+  document.querySelector(".modal").classList.toggle("light");
+  sections.forEach((element) => {
+    element.classList.toggle("light");
+  });
+
+  isDark()
+    ? localStorage.setItem("theme", "dark")
+    : localStorage.setItem("theme", "li ght");
 }
